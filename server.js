d5fe501500304ee419713b8129d8d2c63a160083
Fabioyt <fabiolucaott@gmx.de>
2024-07-31 21:20:11 +0200

    updated  server

--- server.js
+++ server.js
@@ -51,4 +51,11 @@ io.on('connection', (socket) => {
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
   let color = '#';
-  for (let i = 0; i
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
